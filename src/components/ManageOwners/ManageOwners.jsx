c15c09566a61367a959200cc5ca407fd648f9616
pachevang <vangpache@gmail.com>
2019-09-26 10:38:42 -0500

    adds handleChange and handleClick to Manage Owners component

--- src/components/ManageOwners/ManageOwners.jsx
+++ src/components/ManageOwners/ManageOwners.jsx
@@ -3,12 +3,40 @@ import { connect } from 'react-redux';
 // import { HashRouter as Router } from 'react-router-dom';
 
 class ManageOwners extends Component {
+
+    state = {
+        owner: ''
+    }
+
+    handleChangeInput = (event) => {
+        this.setState({
+            owner: event.target.value
+        })
+    }
+
+    handleSubmit = () =>{
+        console.log('submit clicked');
+        
+        this.props.dispatch({
+            type: 'ADD_OWNER',
+            payload: this.state
+        })
+    }
+
+
+
     render() {
         return (
             <>
             <h2>Add Owner</h2>
-            <input placeholder='Owner Name' />
-            <button>SUBMIT</button>
+            <input placeholder='Owner Name'
+                    type="text"
+                    name="owner"
+                    value={this.state.owner}
+                    onChange={(event) => this.handleChangeInput(event)}
+                     />
+            <button onClick={this.handleSubmit}>SUBMIT</button><br/>
+            {JSON.stringify(this.state)}
             <h2>Owners</h2>
             <table>
                 <thead>
