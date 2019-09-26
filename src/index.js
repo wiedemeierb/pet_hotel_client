23c85d598aa0d4b66dda3b19896e68a663538e7f
pachevang <vangpache@gmail.com>
2019-09-26 11:31:44 -0500

    adds POST and GET sagas

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { takeEvery, put } from 'redux-saga/effects'
 import axios from 'axios'
 
-
+///REDUCERS HERE
 const addPetReducer = (state =[], action) => {
     switch (action.type) {
         case 'SET_ALLPETS':
@@ -20,13 +20,72 @@ const addPetReducer = (state =[], action) => {
     }
 }
 
+const getOwnersReducer = (state=[], action) => {
+    switch(action.type) {
+        case 'SET_OWNERS' :
+            return action.payload
+        default:
+            return state;
+    }
+}
+
+
+
+
+///SAGAS HERE
 function* getHistory(action) {
-    let response = yield axios.get('/api/pets/all')
-    console.log('saga getHistory response', response.data)
-    yield put({
-        type: 'SET_ALLPETS',
-        payload: response.data
-    })
+    try {
+        let response = yield axios.get('/api/pets/all')
+        console.log('saga getHistory response', response.data)
+        yield put({
+            type: 'SET_ALLPETS',
+            payload: response.data
+        })
+    } catch (error) {
+        console.log('in getHistory saga error:', error);
+    }
+}
+
+///POST NEW PETS TO DB THEN GET HISTORY OF ALL PETS IN DB
+function* addPet(action) {
+    try {
+        yield axios.post('/api/pets/add', action.payload)
+        console.log('in addPet saga');
+        yield put({
+            type: 'GET_HISTORY'
+        })
+    } catch (error) {
+        console.log('in addPet saga error:', error);
+        
+    }
+    
+}
+
+///GET ALL OWNERS AND NUMBER OF PETS IN DB FOR MANAGE OWNERS VIEW
+function* getOwners(action) {
+    try {
+        let response = yield axios.get('/NEED TO ADD ONCE ESTABLISHED')
+        console.log('in getOwners saga:', response.data);
+        yield put({
+            type: 'SET_OWNERS',
+            payload: response.data
+        })
+    } catch (error) {
+        console.log('in getOwners saga error:', error);  
+    }
+}
+
+///POST NEW OWNERS INTO DATABASE, THEN DISPATCH TO GET ALL OWNERS AND SET IN REDUCER
+function* addOwner(action) {
+    try {
+        yield axios.post('/api/owners/add', action.payload)
+        console.log('in addOwner saga');
+        yield put({
+            type: 'GET_OWNERS'
+        })
+    } catch (error) {
+        console.log('in addOwner saga error:', error);   
+    }
 }
 
 
@@ -36,11 +95,15 @@ const sagaMiddleware = createSagaMiddleware();
 
 function* watcherSaga(){
     yield takeEvery('GET_HISTORY', getHistory)
+    yield takeEvery('GET_OWNERS', getOwners)
+    yield takeEvery('ADD_OWNER', addOwner)
+    yield takeEvery('ADD_PET', addPet)
 }
 
 const store = createStore(
     combineReducers({
-        addPetReducer
+        addPetReducer,
+        getOwnersReducer,
     }),
     applyMiddleware(sagaMiddleware, logger)
 );
