c4a1928a803945be4cff67027592c996ae03a8de
Brandon Wiedemeier <brandon.wiedemeier@gmail.com>
2019-09-26 10:21:56 -0500

    set state and dispatch in Dashboard operational

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -3,18 +3,59 @@ import { connect } from 'react-redux';
 // import { HashRouter as Router } from 'react-router-dom';
 
 class Dashboard extends Component {
+    state = {
+        pet_name: '',
+        pet_color: '',
+        pet_breed: '',
+        owner_name: '',
+    }
+
+    handleChangeForInputs = propertyName => (event) => {
+        this.setState({
+            [propertyName]: event.target.value,
+        });
+        // console.log(this.state);
+    };
+
+    handleSubmit = () => {
+        // console.log('handleSubmit is working')
+        this.props.dispatch({
+            type: 'ADD_PET',
+            payload: this.state
+        })
+    };
+
+    
+
     render(){
         return(
             <>
             <h2>Dashboard Webpage</h2>
             <h2>Add Pet</h2>
-            <input placeholder='Pet Name' />
-            <input placeholder='Pet Color' />
-            <input placeholder='Pet Breed' />
+            {/* {JSON.stringify(this.state)} */}
+            <input 
+                placeholder='Pet Name' 
+                type="text"
+                name="pet_name"
+                value={this.state.pet_name}
+                onChange={this.handleChangeForInputs('pet_name')}
+            />
+            <input 
+                placeholder='Pet Color'
+                type="text"
+                name="pet_color"
+                value={this.state.pet_color}
+                onChange={this.handleChangeForInputs('pet_color')} />
+            <input 
+                placeholder='Pet Breed'
+                type="text"
+                name="pet_breed"
+                value={this.state.pet_breed}
+                onChange={this.handleChangeForInputs('pet_breed')} />
             <select>
                 <option value="Owner"></option>
             </select>
-            <button>SUBMIT</button>
+            <button onClick={this.handleSubmit}>SUBMIT</button>
             <br />
             <h2>History</h2>
             {/* table should eventually have its own component */}
